64f767edfffd0cd2dea0c6dc0aa363ebc093d5ac
강배준 <kbjjun0707@naver.com>
2019-06-30 00:19:55 +0900

    초기 커밋.

---
+++ SizeOfGenericType/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Reflection.Emit;
+
+namespace SizeOfGenericType {
+	public static class Example {
+		public static void SaySizeOfGeneric<T>()
+		{
+			var dm = new DynamicMethod("SizeOfType", typeof(int), new Type[] { });
+			var il = dm.GetILGenerator();
+			il.Emit(OpCodes.Sizeof, typeof(T));
+			il.Emit(OpCodes.Ret);
+			int size = (int)dm.Invoke(null, null);
+
+			Console.WriteLine($"Generic<{typeof(T)}> size : {size}");
+		}
+	}
+
+	public struct Test {
+		int a, b, c;
+	}
+
+	class Program {
+		static void Main(string[] args)
+		{
+			Example.SaySizeOfGeneric<Test>();
+		}
+	}
+}
